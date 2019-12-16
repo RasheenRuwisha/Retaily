bbe9b08f2025ebd6ea0ad7ab53aa1e908683edb9
RasheenRuwisha <sheen.ruwisha@gmail.com>
2019-12-16 18:16:35 +0530

    Added validations to product page add to cart and add review

--- js/ProductPage.js
+++ js/ProductPage.js
@@ -1,47 +1,82 @@
-$(document).ready(function() {
-  var pathname = window.location.pathname;
-  if (pathname.includes("ProductPage")) {
-    loadProduct();
-  }
+$(document).ready(function () {
+    var pathname = window.location.pathname;
+    if (pathname.includes("ProductPage")) {
+        loadProduct();
+        loadReviews();
+
+    }
 });
 
-function loadProduct() {
-  try {
+function loadReviews() {
     var settings = {
-      async: true,
-      crossDomain: true,
-      url:
+        "async": true,
+        "crossDomain": true,
+        "url": "https://retaily-api.herokuapp.com/getReviews?productId=" +
+        sessionStorage.product,
+        "method": "GET",
+        "headers": {
+            "cache-control": "no-cache",
+            "Postman-Token": "02d4562d-1b2e-4a07-aa2f-f546e0a0aa3d"
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        var reviews = "";
+        for (var i = 0; i < response.length; i++) {
+            reviews += `<div class="comment-section-background">
+                <img class="comment-user-icon  product-popup-icon" src="../Resources/images/icons/header-userprofile.png"
+            border="0"/>
+                <p class=" price"><b>${response[i].email}</b></p>
+            <br>
+            <img class="comment-rating-icon  product-popup-icon" src="../Resources/images/icons/Stars png.png"
+            border="0"/>
+                <br>
+                <p class="comment-title price review-title"><b>${response[i].title}</b></p>
+            <br>
+            <p class="comment-description review-comment">${response[i].comment}
+                </p>
+
+                </div>`
+        }
+        $("#reviews").html(reviews);
+        $("#reviews").trigger("create");
+        $("#content").css("display","block");
+        $(".loader").remove();
+    });
+}
+
+function loadProduct() {
+    var settings = {
+        async: true,
+        crossDomain: true,
+        url:
         "https://retaily-api.herokuapp.com/product?productId=" +
         sessionStorage.product,
-      method: "GET",
-      headers: {
-        "cache-control": "no-cache",
-        "Postman-Token": "2203e45e-76d4-4929-bd85-9a0b33a0c78f"
-      }
+        method: "GET",
+        headers: {
+            "cache-control": "no-cache",
+            "Postman-Token": "2203e45e-76d4-4929-bd85-9a0b33a0c78f"
+        }
     };
 
-    $.ajax(settings).done(function(response) {
-      var product = "";
-      product += addProducts(response);
+    $.ajax(settings).done(function (response) {
+        var product = "";
+        product += addProducts(response);
 
-      $(".products-item").html(product);
-      $(".products-item").trigger("create");
-      addProducts();
-      initSlider();
+        $(".products-item").html(product);
+        $(".products-item").trigger("create");
+        addProducts();
+        initSlider();
     });
-  } catch (err) {
-    console.log("loadProducts failed");
-    console.log(err);
-  }
 }
 
 function addProducts(response) {
-  var images = "";
-  var dots = "";
+    var images = "";
+    var dots = "";
 
-  try {
     for (var i = 0; i < response.image.length; i++) {
-      images += `
+        images += `
             <img
                         class="mySlides"
                         src=${response.image[i]}
@@ -49,7 +84,7 @@ function addProducts(response) {
                 />
             `;
 
-      dots += ` <span
+        dots += ` <span
                             class="w3-badge demo w3-border w3-transparent w3-hover-white"
                             onclick="currentDiv(i)"
                     ></span>`;
@@ -104,109 +139,116 @@ function addProducts(response) {
         `;
 
     return productItem;
-  } catch (error) {
-    console.log("addProducts failed");
-    console.log(error);
-  }
+}
+
+
+function addReview(response) {
+
+    var reviewItem =
+        `
+         
+        `
+
+    return reviewItem;
 }
 
 function initSlider() {
-  var slideIndex = 1;
-  showDivs(slideIndex);
+    var slideIndex = 1;
+    showDivs(slideIndex);
 
-  function plusDivs(n) {
-    showDivs((slideIndex += n));
-  }
+    function plusDivs(n) {
+        showDivs((slideIndex += n));
+    }
 
-  function currentDiv(n) {
-    showDivs((slideIndex = n));
-  }
+    function currentDiv(n) {
+        showDivs((slideIndex = n));
+    }
 
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    if (n > x.length) {
-      slideIndex = 1;
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        if (n > x.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = x.length;
+        }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" w3-white", "");
+        }
+        x[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " w3-white";
     }
-    if (n < 1) {
-      slideIndex = x.length;
-    }
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" w3-white", "");
-    }
-    x[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " w3-white";
-  }
 }
 
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs((slideIndex += n));
+    showDivs((slideIndex += n));
 }
 
 function currentDiv(n) {
-  showDivs((slideIndex = n));
+    showDivs((slideIndex = n));
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " w3-white";
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-white";
 }
 
 function incrementQuantity() {
-  var currentQuantity = document
-    .getElementById("productQuantity")
-    .innerHTML.valueOf();
-  currentQuantity++;
-  document.getElementById("productQuantity").innerHTML = currentQuantity;
+    var currentQuantity = document
+        .getElementById("productQuantity")
+        .innerHTML.valueOf();
+    currentQuantity++;
+    document.getElementById("productQuantity").innerHTML = currentQuantity;
 }
 
 function decrementQuantity() {
-  var currentQuantity = document
-    .getElementById("productQuantity")
-    .innerHTML.valueOf();
-  if (!(currentQuantity == 1)) {
-    currentQuantity--;
-    document.getElementById("productQuantity").innerHTML = currentQuantity;
-  }
+    var currentQuantity = document
+        .getElementById("productQuantity")
+        .innerHTML.valueOf();
+    if (!(currentQuantity == 1)) {
+        currentQuantity--;
+        document.getElementById("productQuantity").innerHTML = currentQuantity;
+    }
 }
 
 function addToCart(productid) {
     var value = $("#productQuantity").text();
 
     var settings = {
-      async: true,
-      crossDomain: true,
-      url: "https://retaily-api.herokuapp.com/addToCart",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "cache-control": "no-cache",
-        "Postman-Token": "0db2443e-4c0b-437a-a10e-5e0659219599"
-      },
-      processData: false,
-      data:
+        async: true,
+        crossDomain: true,
+        url: "https://retaily-api.herokuapp.com/addToCart",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache",
+            "Postman-Token": "0db2443e-4c0b-437a-a10e-5e0659219599"
+        },
+        processData: false,
+        data:
         '{\n\t"email":"sheen.ruwisha12@gmail.com",\n\t"productId":"' +
         productid +
         '",\n     "qty":' +
@@ -214,7 +256,78 @@ function addToCart(productid) {
         "\n}\n"
     };
 
-    $.ajax(settings).done(function(response) {
-      console.log(response);
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        if (response === 2005) {
+            $("#cart-success").popup("open");
+            setTimeout(function () {
+                $("#cart-success").popup("close");
+            }, 2000);
+        } else if (response === 3006) {
+            $("#cart-error").popup("open");
+            setTimeout(function () {
+                $("#cart-error").popup("close");
+            }, 2000);
+        }
+    }).error(function () {
+        $("#cart-error").popup("open");
+        setTimeout(function () {
+            $("#cart-error").popup("close");
+        }, 2000);
     });
 }
+
+
+function addReviewProduct() {
+    $("#add-review").popup("open");
+}
+
+
+function addReview() {
+    $("#add-review-btn").css("display", "none");
+    $("#processing-btn").addClass("processing-btn-important");
+
+    var title = $("#title").val().trim();
+    var review = $("#review").val().trim();
+
+    if (title === "") {
+        $("#title").addClass("text-box-error")
+    } else {
+        $("#title").removeClass("text-box-error")
+    }
+
+    if (review === "") {
+        $("#review").addClass("text-box-error")
+    } else {
+        $("#review").removeClass("text-box-error")
+    }
+
+
+    if (review !== "" && title !== "") {
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://retaily-api.herokuapp.com/addReview",
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+                "cache-control": "no-cache",
+                "Postman-Token": "2d243d6a-6eca-4124-ac3c-1a4c84cf7fac"
+            },
+            "processData": false,
+            "data": "{\n\t\"email\":\"sheen.ruwisha@gmail.com\",\n\t\"productId\":\"" + sessionStorage.product + "\",\n\t\"rating\": 4,\n\t\"comment\":\"" + review + "\",\n\t\"title\":\"" + title + "\"\n}"
+        }
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+
+        }).error(function () {
+
+        });
+
+    }
+    $("#processing-btn").removeClass("processing-btn-important");
+    $("#add-review-btn").css("display", "block");
+
+}
+
