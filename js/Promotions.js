a70489dd99f12851b8af95a651a1e561ee52c35d
unknown <ruchilamaditha98@gmail.com>
2019-12-17 13:59:38 +0530

    promotion and payment fixed

--- js/Promotions.js
+++ js/Promotions.js
@@ -1,45 +1,45 @@
 $(document).ready(function() {
-  loadPromotionProducts();
+    loadPromotionProducts();
 });
 
 function loadPromotionProducts() {
-  var promotions = "";
+    var promotions = "";
 
-  try {
-    var settings = {
-      async: true,
-      crossDomain: true,
-      url: "https://retaily-api.herokuapp.com/getPromotions",
-      method: "GET",
-      headers: {
-        "cache-control": "no-cache",
-        "Postman-Token": "bf76efcc-d521-4586-a6cf-db51e1e69225"
-      }
-    };
+    try {
+        var settings = {
+            async: true,
+            crossDomain: true,
+            url: "https://retaily-api.herokuapp.com/getPromotions",
+            method: "GET",
+            headers: {
+                "cache-control": "no-cache",
+                "Postman-Token": "bf76efcc-d521-4586-a6cf-db51e1e69225"
+            }
+        };
 
-    $.ajax(settings).done(function(response) {
-      console.log(response);
-      for (var i = 0; i < response.length; i++) {
-        promotions += addProducts(
-          response[i].promotionId,
-          response[i].name,
-          response[i].description,
-          response[i].image
-        );
-      }
-      $(".loader").remove();
-      $(".products-items").html(promotions);
-      $(".products-items").trigger("create");
-    });
-  } catch (error) {
-    console.log("loadPromotionProducts failed");
-    console.log(error);
-  }
+        $.ajax(settings).done(function(response) {
+            console.log(response);
+            for (var i = 0; i < response.length; i++) {
+                promotions += addProducts(
+                    response[i].promotionId,
+                    response[i].name,
+                    response[i].description,
+                    response[i].image
+                );
+            }
+            $(".loader").remove();
+            $(".products-items").html(promotions);
+            $(".products-items").trigger("create");
+        });
+    } catch (error) {
+        console.log("loadPromotionProducts failed");
+        console.log(error);
+    }
 }
 
 function addProducts(promotionId, name, description, image) {
-  try {
-    var productItem = `
+    try {
+        var productItem = `
         <div class="main">
           <div class="pr-card">
             <div
@@ -47,31 +47,28 @@ function addProducts(promotionId, name, description, image) {
               style="background-color:rgb(209,209,247);"
             >
               <div>
-                <img
-                  src="${image}"
-                  width="100px"
-                  style="margin-left:30%;"
-                />
-              </div>
-              <h6 class="login-labels">${name}</h6>
+                <img class="promotion-card-image"src="${image}"/>
+                <h6 class="promotion-name login-labels">${name}</h6>
               <p class="promotion-text">
               ${description}
               </p>
+              </div>
+              
               <a onclick='addToSessionStorage("${promotionId}")' style="color: #c29a0c;">View</a>
             </div>
           </div>
         </div>
         `;
 
-    return productItem;
-  } catch (error) {
-    console.log("addProdutcs failed");
-    console.log(error);
-  }
+        return productItem;
+    } catch (error) {
+        console.log("addProdutcs failed");
+        console.log(error);
+    }
 }
 
 function addToSessionStorage(promotionId) {
-  console.log(promotionId);
-  sessionStorage.promotion = promotionId;
-  window.location = "PromotionProducts.html";
+    console.log(promotionId);
+    sessionStorage.promotion = promotionId;
+    window.location = "PromotionProducts.html";
 }
